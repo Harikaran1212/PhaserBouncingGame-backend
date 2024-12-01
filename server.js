e030c8b88285548d397f3d5484a1bfba40f075c7
Harikaran1212 <131746803+Harikaran1212@users.noreply.github.com>
2024-12-01 19:22:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
-const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
+const CLIENT_URL = process.env.CLIENT_URL || 'https://silver-cassata-1c58c6.netlify.app/';
 
 const server = http.createServer(app);
 const io = new Server(server, {
